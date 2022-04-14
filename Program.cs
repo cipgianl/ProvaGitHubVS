977b9af022958a58ecd7c5a6b0311d2ce587b30c
Gianluca Cipolletta <cipollettagianluca@gmail.com>
2022-04-14 10:02:22 +0200

    Revert "Aggiornamento del secondo messaggio al program.cs"
    This reverts commit 2041fe60a986b9c35d4926f19a5357b5016072e6.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Secondo Messaggio aggiornato.");
+Console.WriteLine("Eseguito un Revert");
