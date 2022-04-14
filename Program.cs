66ed74f76e6ce72d5f5caae300f99e33b561b64d
Gianluca Cipolletta <cipollettagianluca@gmail.com>
2022-04-14 10:21:04 +0200

    Update output message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Eseguito un Revert");
+Console.WriteLine("Hello Everyone!");
